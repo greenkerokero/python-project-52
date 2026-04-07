a8289eda0786b8752c69280e8d5c761409640166
Dmitriy <kerokero@yandex.ru>
2026-04-07 13:03:47 +0000

    Sort imports

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+
 import dj_database_url
+from dotenv import load_dotenv
 
 load_dotenv()
 
