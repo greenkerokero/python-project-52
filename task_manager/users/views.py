72e0a726a6347e0d11ad287da93158413d6525ae
Dmitriy <kerokero@yandex.ru>
2026-04-15 16:16:34 +0000

    Implement access control for users

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
@@ -23,11 +26,21 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('login')
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = get_user_model()
     form_class = UserForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('users:index')
+
+    def test_func(self):
+        return self.get_object() == self.request.user or self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+
+        messages.error(self.request, "You do not have permission")
+        return redirect('users:index')
 
 
 class UserDeleteView(DeleteView):
