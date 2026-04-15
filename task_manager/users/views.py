a76e13cfd1103f75282e66f687e5a62b308a8e7b
Dmitriy <kerokero@yandex.ru>
2026-04-15 19:04:16 +0000

    implement access control for user deletion

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from task_manager.users.forms import UserForm
 
@@ -49,8 +49,20 @@ class UserUpdateView(
         return redirect('users:index')
 
 
-class UserDeleteView(SuccessMessageMixin, DeleteView):
+class UserDeleteView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView
+):
     model = get_user_model()
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users:index')
     success_message = _('User successfully removed')
+
+    def test_func(self):
+        return self.get_object() == self.request.user or self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+
+        messages.error(self.request, _('You do not have permission to make changes'))
+        return redirect('users:index')
