a8847a58835742728dff0eb0cfc240c2a4f9bd88
Dmitriy <kerokero@yandex.ru>
2026-04-20 13:45:12 +0000

    Make correct update form

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,8 +1,15 @@
-from django.contrib.auth import get_user_model
+from django.forms import ModelForm
+from django.contrib.auth import get_user_model, forms
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(UserCreationForm):
+class UserCreateForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'first_name', 'last_name']
+
+
+class UserUpdateForm(ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name']
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-from task_manager.users.forms import UserForm
+from task_manager.users.forms import UserCreateForm, UserUpdateForm
 from task_manager.users.mixins import UserAccessTestMixin
 
 
@@ -21,7 +21,7 @@ class UserListView(ListView):
 
 class UserCreateView(SuccessMessageMixin, CreateView):
     model = get_user_model()
-    form_class = UserForm
+    form_class = UserCreateForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
     success_message = _('The user has been successfully registered')
@@ -31,7 +31,7 @@ class UserUpdateView(
     UserAccessTestMixin, SuccessMessageMixin, UpdateView
 ):
     model = get_user_model()
-    form_class = UserForm
+    form_class = UserUpdateForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('users:index')
     success_message = _('User successfully updated')
