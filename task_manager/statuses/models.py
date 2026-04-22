66945f9b137c4b65de15ef1cba529d0fcc7d7bf8
Dmitriy <kerokero@yandex.ru>
2026-04-22 17:53:02 +0000

    Fix validation status error message

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -3,9 +3,20 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name=_('Name'))
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_('Name'),
+        error_messages={
+            'unique': _('Status with this Name already exists.')
+        }
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # class Meta:
+    #     verbose_name = _('Status')
+    #     verbose_name_plural = _('Statuses')
 
     def __str__(self):
         return self.name
