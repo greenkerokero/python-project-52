90cce4dd83a71b89d10b88b5bf6a982a49e64736
Dmitriy <kerokero@yandex.ru>
2026-04-23 18:36:35 +0000

    Create index tasks page

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,6 +1,19 @@
 from django.views.generic import ListView
-from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
 
 
 class TasksListView(ListView):
-    pass
+    template_name = 'tasks/index.html'
+
+    def get_queryset(self):
+        return Task.objects.select_related(
+            'status', 'reporter', 'assignee'
+        ).only(
+            'name',
+            'created_at',
+            'status__name',
+            'reporter__first_name',
+            'reporter__last_name',
+            'assignee__first_name',
+            'assignee__last_name',
+        )
