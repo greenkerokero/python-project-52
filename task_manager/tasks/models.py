28ca95a138dfad3e8d8e28ff73eb033436e3e0cc
Dmitriy <kerokero@yandex.ru>
2026-04-23 17:49:13 +0000

    Add Task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from task_manager.statuses.models import Status
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_('Name'),
+        error_messages={'unique': _('Task with this Name already exists.')}
+    )
+    description = models.TextField(blank=True, verbose_name=_('Description'))
+    assignee = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='assigned_tasks',
+        verbose_name=_('Assignee')
+    )
+    reporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='reported_tasks',
+        verbose_name=_('Reporter')
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name='tasks',
+        verbose_name=_('Status')
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Task')
+        verbose_name_plural = _('Tasks')
+
+    def __str__(self):
+        return self.name
