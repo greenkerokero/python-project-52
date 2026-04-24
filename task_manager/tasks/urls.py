6e4d1a00f2faddd64ea1bdd76c77f8c3194762b8
Dmitriy <kerokero@yandex.ru>
2026-04-24 11:41:02 +0000

    Create ticket page

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task_manager.tasks.views import TasksListView
+from task_manager.tasks.views import TasksListView, TasksCreateView
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('', TasksListView.as_view(), name='index'),
+    path('create/', TasksCreateView.as_view(), name='create'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from task_manager.tasks.forms import TaskForm
+from task_manager.tasks.mixins import LoginRequiredMessagesMixin
 from task_manager.tasks.models import Task
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 
 
 class TasksListView(ListView):
@@ -17,3 +23,15 @@ class TasksListView(ListView):
             'assignee__first_name',
             'assignee__last_name',
         )
+
+
+class TasksCreateView(LoginRequiredMessagesMixin, SuccessMessageMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/create.html'
+    success_url = reverse_lazy('tasks:index')
+    success_message = _('Task successfully created')
+
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
