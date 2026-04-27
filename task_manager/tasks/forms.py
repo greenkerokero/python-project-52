fc5a85e1991d48f3da28d60a13211058b125c6e8
Dmitriy <kerokero@yandex.ru>
2026-04-27 09:25:56 +0000

    improve user display name in navigation and assignee choice field

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -7,3 +7,11 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assignee']
+
+    def __init__(self, *args, **kwargs):
+        # 1. Сначала вызываем стандартную инициализацию формы
+        super().__init__(*args, **kwargs)
+
+        # 2. Подменяем функцию генерации имени (label) для поля assignee
+        # Теперь форма будет вызывать get_full_name() у каждого юзера
+        self.fields['assignee'].label_from_instance = lambda obj: obj.get_full_name() or obj.username
