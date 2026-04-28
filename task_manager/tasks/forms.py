d93d066f26e1d8126eb9da2d5ac5c16b7f81ca33
Dmitriy <kerokero@yandex.ru>
2026-04-28 12:28:00 +0000

    Add label field to tasks pages

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -6,14 +6,11 @@ from task_manager.tasks.models import Task
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'status', 'assignee']
+        fields = ['name', 'description', 'status', 'assignee', 'labels']
 
     def __init__(self, *args, **kwargs):
-        # 1. Сначала вызываем стандартную инициализацию формы
         super().__init__(*args, **kwargs)
 
-        # 2. Подменяем функцию генерации имени (label) для поля assignee
-        # Теперь форма будет вызывать get_full_name() у каждого юзера
         self.fields['assignee'].label_from_instance = lambda obj: (
-            obj.get_full_name() or obj.username
+                obj.get_full_name() or obj.username
         )
