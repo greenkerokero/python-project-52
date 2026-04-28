24fe42d76555b74c0376d1241585f73ca1b95591
Dmitriy <kerokero@yandex.ru>
2026-04-28 13:44:56 +0000

    Create status adding functionality

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task_manager.labels.views import LabelListView
+from task_manager.labels.views import LabelListView, LabelCreateView
 
 app_name = 'labels'
 
 urlpatterns = [
     path('', LabelListView.as_view(), name='index'),
+    path('create/', LabelCreateView.as_view(), name='create'),
 ]
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,6 +1,12 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from task_manager.labels.models import Label
 from task_manager.labels.mixin import LoginRequiredMessagesMixin
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.labels.forms import LabelForm
 
 
 class LabelListView(LoginRequiredMessagesMixin, ListView):
@@ -8,3 +14,11 @@ class LabelListView(LoginRequiredMessagesMixin, ListView):
 
     def get_queryset(self):
         return Label.objects.only('id', 'name', 'created_at').order_by('created_at')
+
+
+class LabelCreateView(LoginRequiredMessagesMixin, SuccessMessageMixin, CreateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/create.html'
+    success_url = reverse_lazy('labels:index')
+    success_message = _('Label successfully created')
