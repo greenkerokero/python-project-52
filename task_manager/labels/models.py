cead68d5ce3798034511d5bfb0e95effcb2fadd9
Dmitriy <kerokero@yandex.ru>
2026-04-28 12:22:37 +0000

    Add Label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_('Name'),
+    )
+
+    def __str__(self):
+        return self.name
