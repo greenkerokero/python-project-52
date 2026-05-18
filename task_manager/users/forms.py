d6a0a373f9c17db1aa3a4fc28b867729e9a05c2e
Dmitriy <kerokero@yandex.ru>
2026-05-18 12:56:20 +0000

    Fix lint errors

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
 
 class UserCreateForm(UserCreationForm):
