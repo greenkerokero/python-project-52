cc0e369603c03938be7e08b0b9607128113f0eef
Dmitriy <kerokero@yandex.ru>
2026-05-18 14:18:39 +0000

    Fix settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -118,6 +118,10 @@ AUTH_USER_MODEL = 'users.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation"
                 ".MinimumLengthValidator",
     },
     {
