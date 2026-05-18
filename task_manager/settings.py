bf05ff0f34ef3d7f8c2927f1756982aabc5e6835
Dmitriy <kerokero@yandex.ru>
2026-05-18 12:13:02 +0000

    Fix password validators settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation'
                 '.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 5,
+            'min_length': 4,
         }
     },
     {
