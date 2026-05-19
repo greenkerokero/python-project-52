926d71070a8f40bada03ae5200429618a2fbad05
Dmitriy <kerokero@yandex.ru>
2026-05-19 09:36:30 +0000

    Fix linter errors

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Task
+
+from task_manager.tasks.models import Task
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'status', 'reporter', 'assignee',
-                    'created_at')
+    list_display = (
+        'id',
+        'name',
+        'status',
+        'reporter',
+        'assignee',
+        'created_at',
+    )
     search_fields = ('name', 'description')
     list_filter = ('status', 'reporter', 'assignee', 'labels')
     list_display_links = ('id', 'name')
